4cdd55c1b96fb4a6b75c2e18c00a9c0da146a2a5
EiramCloset <marieyankey@hotmail.com>
2026-09-14 16:47:27 +0200

    Refresh cache for smarter outfit engine

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='eiram-closet-v10';
+const CACHE='eiram-closet-v11';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./enhancements.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()]))});
